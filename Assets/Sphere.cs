3bc722b900c84804dad68c496810b4747736a137
Garrett Radtke <60516972+GarrettRadtke@users.noreply.github.com>
2020-02-24 10:30:16 -0600

    Starting out practicing for the game
    yes

---
+++ Assets/Sphere.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -22)
+        {
+            Respawn();
+        }
+    }
+    private void OnEnable()
+    {
+        Respawn();
+    }
+
+    private void Respawn()
+    {
+        float randomY = UnityEngine.Random.Range(1, 15);
+        float randomX = UnityEngine.Random.Range(-12, 12);
+
+        transform.position = new Vector3(randomX, randomY);
+
+        var rigibbody = GetComponent<Rigidbody>();
+    }
+}
